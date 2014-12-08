7ec993d941b5cbee992c2a9003033562aa4f1cfb
Matthias Ludwig <matt.ludwig@me.com>
2014-12-08 19:01:20 +0100

    - removed tests
    - implemented jid-api from server

---
+++ ChatSecure/Classes/View Controllers/Login View Controllers/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  messenger7
+//
+//  Created by Matthias Ludwig on 08.12.14.
+//  Copyright (c) 2014 Chris Ballinger. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
